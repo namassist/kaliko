bfa499bdc10592779d63ad31b51f84dc9a32f7dd
namassist <chairulanmm@gmail.com>
2024-12-22 19:17:45 +0700

    ✨ feat: Add SplashScreen widget with delay navigation feature.

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, '/login');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/splash-logo.png',
+              width: 235,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
